7c8cd0c071538161c5da804150071df508e3bbb9
happycod3r <happy2code0101@gmail.com>
2023-07-31 22:46:58 -0400

    [chore] updated gitignore

--- test.py
+++
@@ -1,8 +0,0 @@
-import youtube_api_tools
-
-tube = youtube_api_tools.YouTubeAPIClientTools(
-    "client_secret_671382908634-hudnrlsnhr3gqresomjga29a33s0chml.apps.googleusercontent.com.json", 
-    ["https://www.googleapis.com/auth/youtube.readonly"]
-)
-rgr_id = "UCZiVfeN-xX_U-xDyDipTB-A"
-tube.get_channel_info(rgr_id)
